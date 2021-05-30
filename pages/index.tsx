a7c63a430169d25fe8b3d7ae7be840303fb5ea71
Guillermo <guilleandmont@gmail.com>
2021-05-30 08:22:37 -0400

    deleted default styles and got tailwind working

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="text-bold text-indigo-800 text-3xl p-4">
+      Welcome to my future portfolio
+    </div>
+  );
+}
