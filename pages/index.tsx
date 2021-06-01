c69aa05abefdee10ea239f9f4057bd32119ef4c3
Guillermo <guilleandmont@gmail.com>
2021-06-01 07:28:32 -0400

    added customfont

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,11 @@ import Layout from "../components/Layout/Layout";
 export default function Home() {
   return (
     <Layout>
-      <header className="flex flex-row w-full justify-between">
+      <header className="flex flex-row w-full justify-between items-center">
         <div className="w-10 h-10 bg-red-800"></div>
         <Nav />
       </header>
-      <p className="text-red-700 text-xl">Welcome to my future portfolio</p>
+      <p className="text-red-200 text-xl">Welcome to my future portfolio</p>
     </Layout>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false, // or 'media' or 'class
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Fira Sans", ...defaultTheme.fontFamily.sans],
+      mono: ["Fira Code", defaultTheme.fontFamily.mono],
+    },
   },
   variants: {
     extend: {},
